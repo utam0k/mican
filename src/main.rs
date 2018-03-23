158d847d8a19d83504ca88a8f1193fd6835e30de
utam0k <k0ma@utam0k.jp>
2018-03-24 01:03:24 +0900

    fix cd

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,27 @@ enum Command {}
 
 fn rtsh_cd(args: Vec<&str>) -> Result<(), String> {
     if args.len() < 2 {
-        // env::set_current_dir(&env::home_dir().unwrap()).unwrap();
-        // use std::path::Path;
-        // env::set_current_dir(Path::new("/home/utam0k/")).unwrap();
-        unsafe {
-            if libc::chdir(CString::new("/home/utam0k").unwrap().as_ptr()) == -1 {
-                return Err(format!("{} not found", args[1]));
-            };
-        }
+        env::set_current_dir(&env::home_dir().unwrap()).unwrap();
         return Ok(());
     }
-    // let mut current_path_buf = std::env::current_exe().unwrap();
-    // current_path_buf.push(args[1]);
-    // println!("{:?}", current_path_buf);
-    // if env::set_current_dir(current_path_buf.as_path()).is_err(){
-    //     return Err(format!("{} not found", args[1]));
-    // };
-    // return Ok(());
-    unsafe {
-        if libc::chdir(CString::new(args[1]).unwrap().as_ptr()) == -1 {
-            return Err(format!("{} not found", args[1]));
-        };
-    }
+
+    let mut current_path_buf = std::env::current_dir().unwrap();
+    current_path_buf.push(args[1]);
+    if env::set_current_dir(current_path_buf.as_path()).is_err(){
+        return Err(format!("{} not found", args[1]));
+    };
     return Ok(())
 }
 
 fn rtsh_ls(args: Vec<&str>) -> Result<(), String> {
-    for entry in fs::read_dir(env::current_exe().unwrap()).unwrap() {
+    for entry in fs::read_dir(env::current_dir().unwrap()).unwrap() {
         println!("{:?}", entry.unwrap().file_name());
     }
     return Ok(());
 }
 
 fn rtsh_pwd() -> Result<(), String> {
-    println!("{:?}", env::current_exe().unwrap());
+    println!("{:?}", env::current_dir().unwrap());
     return Ok(());
 }
 
@@ -62,10 +49,10 @@ fn main() {
         let args: Vec<&str> = cmd.split(" ").collect();
 
         let _ = match args[0] {
-            "cd" => rtsh_cd(args),
-            "ls" => rtsh_ls(args),
+            "cd"  => rtsh_cd(args),
+            "ls"  => rtsh_ls(args),
             "pwd" => rtsh_pwd(),
-            _    => Err(format!("not found {} command.", cmd)),
+            _     => Err(format!("not found {} command.", cmd)),
         }.map_err(|err| eprintln!("{}", err));
     }
 }
