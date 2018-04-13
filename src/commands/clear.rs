42454177a8a2f4e64cd48dba1be1da8154a9dbff
utam0k <k0ma@utam0k.jp>
2018-04-13 21:40:12 +0900

    clear command added test

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -7,3 +7,17 @@ pub fn run(cmd: CommandData) -> Result<(), String> {
     out.write_all("\x1b[2J\x1b[1;1H".as_bytes()).unwrap();
     return Ok(());
 }
+
+#[test]
+fn test_bye_run() {
+    use std::fs::File;
+
+    let cmd = CommandData {
+        program: "clear".to_string(),
+        options: vec![],
+        out: Some(File::create("/dev/null").unwrap()),
+        input: None,
+    };
+
+    assert!(run(cmd).is_ok());
+}
