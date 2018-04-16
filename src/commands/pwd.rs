c743f5cb4b94c129d8b10c8afb63278578860c6b
utam0k <k0ma@utam0k.jp>
2018-04-16 20:21:42 +0900

    remove the double quotation marks at the start and end of display of pwd

--- src/commands/pwd.rs
+++ src/commands/pwd.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::io::prelude::*;
 
 pub fn run(cmd: CommandData) -> Result<(), String> {
-    let result = format!("{:?}\n", env::current_dir().unwrap());
+    let result = format!("{}\n", env::current_dir().unwrap().display());
     let mut out = cmd.out.unwrap();
     match out.write_all(result.as_bytes()) {
         Ok(_) => Ok(()),
