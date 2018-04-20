46d20a7966b6ff88263e58d84d1b743f6d318924
utam0k <k0ma@utam0k.jp>
2018-04-21 00:00:40 +0900

    change bye command to kill parent process

--- src/commands/bye.rs
+++ src/commands/bye.rs
@@ -3,11 +3,17 @@ use token::CommandData;
 use std::io::prelude::*;
 use std::process::exit;
 
+use nix::unistd::getppid;
+use nix::sys::signal::{kill, Signal};
+
 pub fn run(cmd: CommandData) -> Result<(), String> {
-    let result = "Thank you for using Mican🍊\n";
+    let result = "Thank you for using Mican🍊";
     let mut out = cmd.out.unwrap();
     match out.write_all(result.as_bytes()) {
         Ok(_) => {
+            // TODO
+            // kill(getppid(), Signal::SIGCHLD).unwrap();
+            kill(getppid(), Signal::SIGINT).unwrap();
             exit(1);
         }
         Err(_) => Err("Error: bye".to_string()),
