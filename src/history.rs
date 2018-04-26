2d1ffd687f7d300886e4897ffcbc6f3f285ffb60
utam0k <k0ma@utam0k.jp>
2018-04-26 16:29:28 +0900

    fix bug of history order

--- src/history.rs
+++ src/history.rs
@@ -1,37 +1,67 @@
+use std::collections::VecDeque;
+
 pub struct History {
-    pub list: Vec<String>,
+    pub list: VecDeque<String>,
     pub pos: usize,
+    prev: HistoryExec,
+}
+
+#[derive(PartialEq)]
+enum HistoryExec {
+    Next,
+    Prev,
 }
 
 impl History {
     pub fn new() -> History {
         History {
-            list: Vec::new(),
+            list: VecDeque::new(),
             pos: 0,
+            prev: HistoryExec::Prev,
         }
     }
 
     pub fn next(&mut self) -> Option<&String> {
-        let ret = self.list.get(self.pos);
-        if ret.is_some() {
-            self.pos -= 1;
+        if self.pos == 0 {
+            return None;
         }
+        if self.prev == HistoryExec::Prev {
+            self.pos -= 1;
+            self.prev = HistoryExec::Next;
+        }
+        self.pos -= 1;
+        let ret = self.list.get(self.pos);
         ret
     }
 
     pub fn prev(&mut self) -> Option<&String> {
-        let ret = self.list.get(self.pos);
-        if ret.is_some() {
+        if self.prev == HistoryExec::Next {
             self.pos += 1;
+            self.prev = HistoryExec::Prev;
         }
+        let ret = self.list.get(self.pos);
+        if ret.is_none() {
+            return None;
+        }
+        self.pos += 1;
         ret
     }
 
     pub fn push(&mut self, s: String) {
-        self.list.push(s);
+        self.list.push_front(s);
     }
+}
 
-    // pub fn is_start(&self) -> bool {
-    //     self.pos == 0
-    // }
+#[test]
+fn test_history() {
+    let mut history = History::new();
+    history.push("A".to_string());
+    history.push("B".to_string());
+    history.push("C".to_string());
+
+    assert_eq!(history.prev(), Some(&"B".to_string()));
+    assert_eq!(history.prev(), Some(&"A".to_string()));
+    assert_eq!(history.prev(), None);
+    assert_eq!(history.next(), Some(&"B".to_string()));
+    assert_eq!(history.next(), Some(&"C".to_string()));
 }
