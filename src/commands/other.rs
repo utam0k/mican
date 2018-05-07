b1729e79e0885bd059b0b862d2d4bf6b5e98bb0c
utam0k <k0ma@utam0k.jp>
2018-05-07 23:08:28 +0900

    set stderr of other command

--- src/commands/other.rs
+++ src/commands/other.rs
@@ -1,6 +1,11 @@
 use token::CommandData;
 
+use nix::unistd::dup;
+use libc::STDOUT_FILENO;
+
 use std::process::Command;
+use std::os::unix::io::FromRawFd;
+use std::fs;
 
 pub fn run(cmd: CommandData) -> Result<(), String> {
     let input = cmd.input.unwrap();
@@ -10,6 +15,9 @@ pub fn run(cmd: CommandData) -> Result<(), String> {
         .args(&cmd.options)
         .stdin(input)
         .stdout(out)
+        .stderr(unsafe {
+            fs::File::from_raw_fd(dup(STDOUT_FILENO).unwrap())
+        })
         .spawn() {
         Ok(p) => p,
         Err(e) => return Err(format!("{}", e)),
