72d4b1523dd75949ff4efa3751e97b8878afe0e6
utam0k <k0ma@utam0k.jp>
2018-05-30 17:50:25 +0900

    change a kill signal of bye command

--- src/commands/bye.rs
+++ src/commands/bye.rs
@@ -7,15 +7,14 @@ use nix::unistd::getppid;
 use nix::sys::signal::{kill, Signal};
 
 pub fn run(cmd: CommandData) -> Result<(), String> {
-    // let result = "Thank you for using Mican🍊";
-    let result = "Thank you for using Mican\u{1f34a}";
+    let result = "Thank you for using Mican\u{1f34a}\n"; // Thank you for using Mican🍊\n
     let mut out = cmd.out.unwrap();
     match out.write_all(result.as_bytes()) {
         Ok(_) => {
             // TODO
             // kill(getppid(), Signal::SIGCHLD).unwrap();
             out.flush().unwrap();
-            kill(getppid(), Signal::SIGINT).unwrap();
+            kill(getppid(), Signal::SIGKILL).unwrap();
             exit(0);
         }
         Err(_) => Err("Error: bye".to_string()),
